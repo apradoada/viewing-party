84e842f28490f869ae212e6383f70a7cd4efbb08
Adrian <adrian.prado@adadevelopersacademy.org>
2022-09-22 10:42:21 -0500

    Pass all tests wave 2

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -33,7 +33,37 @@ def watch_movie(user_data, title):
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
 
+def get_watched_avg_rating(user_data):
+#
+    total = 0.0
+    if len(user_data["watched"]) == 0:
+        return total
 
+    for movie in user_data["watched"]:
+        total += movie["rating"]
+    
+    return total/len(user_data["watched"])
+
+def get_most_watched_genre(user_data):
+    genre_count = {}
+    max = 0
+    max_genre = ""
+
+    if len(user_data["watched"]) == 0:
+        return None
+
+    for movie in user_data["watched"]:
+        if movie["genre"] not in genre_count:
+            genre_count[movie["genre"]] = 1
+        else:
+            genre_count[movie["genre"]] = genre_count[movie["genre"]] + 1
+    
+    for genre in genre_count:
+        if(genre_count[genre] > max):
+            max = genre_count[genre]
+            max_genre = genre
+
+    return max_genre
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
